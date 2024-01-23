c895f855355fef420536bd2024d4ea3439f39e11
enejivk <enejivic@gmail.com>
2024-01-23 12:09:14 +0100

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,13 +1,27 @@
+#include "sort.h"
 /**
-*selection_sort- Sorting using selection
-*@array: The array to be sorted
-*@size: The size of the array
-*/
+ *selection_sort- Sorting using selection
+ *@array: The array to be sorted
+ *@size: The size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-int i, j, temp, min;
-for (i = 0; i < size; i++)
-{
-min = 1;
-}
+	size_t i, j, temp, min;
+
+	if (!array || !size)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+		print_array(array, size);
+	}
 }
